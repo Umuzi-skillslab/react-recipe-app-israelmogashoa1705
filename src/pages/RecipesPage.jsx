ed92a587e9c9e0fd95e147a79c7456e52b4d2926
Mogau Mogashoa <israelmogashoa@gmail.com>
2026-09-05 16:52:26 +0200

    Implemented a function that filters recipe

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -69,6 +69,16 @@ const RecipesPage = ({
     sortOption,
   ]);
 
+  const handleClearFilters = () => {
+    setSearchTerm('');
+    setSelectedCategory('all');
+    setSelectedCuisine('all');
+    setSelectedDifficulty('all');
+    setSortOption('title');
+  };
+
+  
+
 
 
 
