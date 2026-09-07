b1101ca4acaec486e1c1db615bb36158ae0f1de4
Mogau Mogashoa <israelmogashoa@gmail.com>
2026-09-07 19:39:43 +0200

    Added PageNotFound component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../components/UI/Button';
+import Card from '../components/UI/Card';
+
+const NotFound = () => {
+  const navigate = useNavigate();
