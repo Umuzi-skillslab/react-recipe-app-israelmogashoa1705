4fd075ebbc0cc0838519d55420d9aa4b672c5e44
Mogau Mogashoa <israelmogashoa@gmail.com>
2026-09-08 18:07:02 +0200

    Updated version

--- src/components/UI/SearchBar.jsx
+++ src/components/UI/SearchBar.jsx
@@ -28,35 +28,4 @@ const SearchBar = ({
         Search recipes
       </label>
 
-/*
-
-      <input
-        id="recipe-search"
-        type="text"
-        value={searchTerm}
-        placeholder={placeholder}
-        onChange={handleChange}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        aria-label="Search recipes"
-      />
-
-      {hasSubmitted && (
-        <p className="search-feedback">
-          Searching for{' '}
-          <strong>{searchTerm || 'all recipes'}</strong>
-        </p>
-      )}
-    </form>
-  );
-};
-
-SearchBar.propTypes = {
-  searchTerm: PropTypes.string.isRequired,
-  onSearch: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-};
-
-export default SearchBar;
-
 
